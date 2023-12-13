b90fc95951a2b5f8873427a8f512e023cbe6a6d1
lampman <lampmanyao@gmail.com>
2023-12-13 19:37:51 +0800

    update price

---
+++ script.js
@@ -0,0 +1,12 @@
+let openHam = document.querySelector('#openHam');
+let closeHam = document.querySelector('#closeHam');
+let navigationItems = document.querySelector('#navigation-items');
+
+const hamburgerEvent = (navigation, close, open) => {
+    navigationItems.style.display = navigation;
+    closeHam.style.display = close;
+    openHam.style.display = open;
+};
+
+openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
+closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
